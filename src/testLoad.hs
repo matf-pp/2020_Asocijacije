05e65295e3c3989e6f8287d0d7ca1239a37c9d16
Nikola Ubavić <ubavic>
2020-04-28 19:46:51 +0200

    Malo ciscenje

--- src/testLoad.hs
+++
@@ -1,9 +0,0 @@
-import qualified LoadAssociation 
-import qualified LoadSettings
-
-main = do
---     wordAssociationTableContentObject <- LoadAssociation.makeNewWordAssociation
---     print $ LoadAssociation.getItem "a2" wordAssociationTableContentObject
-    settingsObject <- LoadSettings.readSettingsFile
-    print $ LoadSettings.getItem "igrac1_ime" settingsObject
-    
